522d2f5eda42114a3aa967cb354fa98a35a22f19
Darshil <Darshilkhunt9999@gmail.com>
2021-01-14 18:37:16 -0500

    Initial commit

---
+++ app/src/main/java/com/cst2355/khun0008/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cst2355.khun0008;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
