7fc4ca68ed521a30b929b2320bcb3612781a93f0
Darshil <Darshilkhunt9999@gmail.com>
2021-01-27 20:44:23 -0500

    working on linear layout

--- app/src/main/java/com/cst2355/khun0008/MainActivity.java
+++ app/src/main/java/com/cst2355/khun0008/MainActivity.java
@@ -3,12 +3,21 @@ package com.cst2355.khun0008;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        Button myBtn = findViewById(R.id.button2);
+        myBtn.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+        });
+
     }
 }
