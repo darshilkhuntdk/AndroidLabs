ffd14cdc263e8d3fe69eaa232c3aee9d13789f2c
Darshil <Darshilkhunt9999@gmail.com>
2021-02-03 19:58:56 -0500

    working on grid layout

--- app/src/main/java/com/cst2355/khun0008/MainActivity.java
+++ app/src/main/java/com/cst2355/khun0008/MainActivity.java
@@ -3,21 +3,35 @@ package com.cst2355.khun0008;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         Button myBtn = findViewById(R.id.button2);
         myBtn.setOnClickListener(v -> {
             Toast.makeText(MainActivity.this, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
         });
+
+        Switch onOffSwitch = findViewById(R.id.switch1);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Snackbar.make(onOffSwitch,isChecked?"The switch is now on":"The switch is now off",Snackbar.LENGTH_LONG).show();
+                //Snackbar.setAction("UNDO",click -> buttonView.setChecked(!isChecked));
+            }
+        });
+
+
 
     }
 }
