542634bfa7c20be32e366dcfb231ff48793458ad
Darshil <Darshilkhunt9999@gmail.com>
2021-02-10 19:01:07 -0700

    lab 3

--- app/src/main/java/com/cst2355/khun0008/MainActivity.java
+++ app/src/main/java/com/cst2355/khun0008/MainActivity.java
@@ -2,22 +2,36 @@ package com.cst2355.khun0008;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences prefs = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
 
-        Button myBtn = findViewById(R.id.button2);
-        myBtn.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
-        });
+        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+
+        String savedString = prefs.getString("Email", "");
+        EditText email = findViewById(R.id.edittext1);
+        email.setText(savedString);
+
+        Button loginButton = findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(bt -> saveSharedPrefs(email.getText().toString()));
 
     }
+
+    private void saveSharedPrefs(String stringToSave) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("Email", stringToSave);
+        editor.commit();
+        }
+
 }
---
+++ app/src/main/java/com/cst2355/khun0008/ProfileActivity.java
@@ -0,0 +1,39 @@
+package com.cst2355.khun0008;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ImageButton iButton = findViewById(R.id.imageButton);
+        iButton.setOnClickListener((clk) -> {
+            dispatchTakePictureIntent();
+        });
+
+        }
+        static final int REQUEST_IMAGE_CAPTURE = 1;
+        private void dispatchTakePictureIntent () {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
+
