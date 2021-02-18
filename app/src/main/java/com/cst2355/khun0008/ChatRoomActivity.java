6b5d7665e11dd7e255a03d8c444fe9b2571c7aae
Darshil <Darshilkhunt9999@gmail.com>
2021-02-18 00:15:11 -0500

    Lab4_final

--- app/src/main/java/com/cst2355/khun0008/ChatRoomActivity.java
+++ app/src/main/java/com/cst2355/khun0008/ChatRoomActivity.java
@@ -21,30 +21,44 @@ public class ChatRoomActivity extends AppCompatActivity {
     private Message message = new Message();
     private ArrayList<String> elements = new ArrayList<>(Arrays.asList());
     private MyListAdapter myAdapter;
+    private EditText textValue;
+    private boolean sendOrReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
+        textValue = findViewById(R.id.messageTypeArea);
+
+
         Button sendButton = findViewById(R.id.messageSendButton);
         Button receiveButton = findViewById(R.id.messageReceiveButton);
-        sendButton.setOnClickListener((clk)->
+        sendButton.setOnClickListener((clk) ->
         {
             elements.add(message.getMessage());
             myAdapter.notifyDataSetChanged();
+            textValue.getText().clear();
+            sendOrReceive = false;
+        });
+        receiveButton.setOnClickListener((clk) ->
+        {
+            elements.add(message.getMessage());
+            myAdapter.notifyDataSetChanged();
+            textValue.getText().clear();
+            sendOrReceive = true;
         });
 
 
         ListView myList = findViewById(R.id.messages_list);
         myList.setAdapter(myAdapter = new MyListAdapter());
 
-        myList.setOnItemLongClickListener( (p, b, pos, id) -> {
+        myList.setOnItemLongClickListener((p, b, pos, id) -> {
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setTitle("Do you want to delete this?")
 
                     //What is the message:
-                    .setMessage("The selected row is: " +pos +"/nThe database id is:" +id)
+                    .setMessage("The selected row is: " + pos + "\nThe database id is:" + id)
 
                     //what the Yes button does:
                     .setPositiveButton("Positive", (click, arg) -> {
@@ -52,7 +66,8 @@ public class ChatRoomActivity extends AppCompatActivity {
                         myAdapter.notifyDataSetChanged();
                     })
                     //What the No button does:
-                    .setNegativeButton("Negative", (click, arg) -> { })
+                    .setNegativeButton("Negative", (click, arg) -> {
+                    })
 
                     //An optional third button:
                     //.setNeutralButton("Maybe", (click, arg) -> {  })
@@ -67,8 +82,7 @@ public class ChatRoomActivity extends AppCompatActivity {
     }
 
     private class Message {
-        EditText textValue = findViewById(R.id.messageTypeArea);
-        public String getMessage(){
+        public String getMessage() {
             return textValue.getText().toString();
         }
     }
@@ -79,19 +93,29 @@ public class ChatRoomActivity extends AppCompatActivity {
         }
 
         public Object getItem(int position) {
-            return message.getMessage();
+            return elements.get(position);
         }
 
         public long getItemId(int p) {
-            return (long)p;
+            return (long) p;
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getLayoutInflater();
-            View newRow = inflater.inflate(R.layout.row_send, parent, false);
-            TextView textViewOfReceive = newRow.findViewById(R.id.textViewSend);
-            textViewOfReceive.setText((String)getItem(position));
-            return newRow;
+            View v;
+                if (sendOrReceive == false) {
+                    View newRowSend = inflater.inflate(R.layout.row_send, parent, false);
+                    TextView textViewOfSend = newRowSend.findViewById(R.id.textViewSend);
+                    textViewOfSend.setText((String) getItem(position));
+                    v = newRowSend;
+                    return v;
+                } else {
+                    View newRowReceive = inflater.inflate(R.layout.row_receive, parent, false);
+                    TextView textViewOfReceive = newRowReceive.findViewById(R.id.textViewReceive);
+                    textViewOfReceive.setText((String) getItem(position));
+                    v = newRowReceive;
+                    return v;
+                }
         }
     }
 }
